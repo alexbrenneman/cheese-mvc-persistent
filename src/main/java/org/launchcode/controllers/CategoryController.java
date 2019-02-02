5739d9a2c4f056bff3b33956a02a0cd83e20b07b
Alex Brenneman <alexqbrenneman@gmail.com>
2019-02-02 16:16:14 -0600

    Got part 1 working as it should

--- src/main/java/org/launchcode/controllers/CategoryController.java
+++ src/main/java/org/launchcode/controllers/CategoryController.java
@@ -1,5 +1,7 @@
 package org.launchcode.controllers;
 
+
+import org.launchcode.models.Category;
 import org.launchcode.models.data.CategoryDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -8,42 +10,50 @@ import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 
 @Controller
 @RequestMapping("category")
 public class CategoryController {
+
     @Autowired
-    private CategoryDao categoryDao;
+    CategoryDao categoryDao;
 
-    //Request path: /category
+    @RequestMapping(value = "", method = RequestMethod.GET)
+    public String index(Model model, @RequestParam(defaultValue = "0") int id) {
 
-    @RequestMapping(value = "")
-    public String index(Model model) {
+        model.addAttribute("title", "Categories");
         model.addAttribute("categories", categoryDao.findAll());
-        model.addAttribute("title", "Category");
+
         return "category/index";
+
     }
 
     @RequestMapping(value = "add", method = RequestMethod.GET)
     public String add(Model model) {
+
         model.addAttribute(new Category());
         model.addAttribute("title", "Add Category");
-        return "controller/add";
+
+        return "category/add";
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model,
-                      @ModelAttribute @Valid Category category, Errors errors) {
+    public String add(Model model, @ModelAttribute @Valid Category category, Errors errors) {
+
         if (errors.hasErrors()) {
+
             model.addAttribute("title", "Add Category");
+
             return "category/add";
+
         }
 
         categoryDao.save(category);
         return "redirect:";
 
-
     }
+
 }
